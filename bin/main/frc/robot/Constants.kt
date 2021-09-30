81fffd9accd52ec7f1125cbd8ffa3ff4c37f2b13
James Ding <48845764+twango-dev@users.noreply.github.com>
2021-09-29 22:10:19 -0700

    Initial commit

---
+++ bin/main/frc/robot/Constants.kt
@@ -0,0 +1,17 @@
+package frc.robot
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants.  This class should not be used for any other purpose.  All constants should be
+ * declared globally. Do not put anything functional in this class.
+ *
+ * The default generated template generated a class, with a companion object. In this case we will
+ * be using an object to reduce verbosity.
+ *
+ */
+object Constants {
+
+
+
+}
+
